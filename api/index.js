79711660d65ec4c1465d684019b0d76e79814540
ZakBrindle <z4kbrindle@gmail.com>
2023-07-19 19:21:59 +0100

    stripe changes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 // This is your test secret API key.
-const stripe = require('stripe')('');
+const stripe = require('stripe')('pk_live_51NVd2MEjWpAK8TuWU2ViGWscfzmVYt7KvTy2UoRWYR6KwJapFdGIwp3gzfZVnr8LPyqYhrOuoN3IVVof2J2NAqMW00GYLxjotP');
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
@@ -7,6 +7,8 @@ app.use(express.static('public'));
 const YOUR_DOMAIN = 'http://localhost:4242';
 
 app.post('/create-checkout-session', async (req, res) => {
+console.log("Making STRIPE request...");
+
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
