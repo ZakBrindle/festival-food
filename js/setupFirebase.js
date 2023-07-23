f27801ca15a46eb0fa0a41615a4ca9a89c73873d
ZakBrindle <z4kbrindle@gmail.com>
2023-07-23 16:00:26 +0100

    firebase initial 5

--- js/setupFirebase.js
+++ js/setupFirebase.js
@@ -13,8 +13,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
+console.log("initialise firebase");
 async function getData(app)
 {
+  
  return;
 }
